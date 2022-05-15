453ce8699077adc780e12351858bb5604b144577
Naben <76815289+Naben-dev@users.noreply.github.com>
2022-05-15 05:12:40 +0200

    Update fxmanifest.lua

--- nRenfort/fxmanifest.lua
+++ nRenfort/fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version 'cerulean'
 games {'gta5'}
 
+author 'Naben'
+description 'Menu demande de Renfort LSPD'
+version '1.0.0'
+
 ui_page 'data/index.html'
 
 client_script 'client/client.lua'
